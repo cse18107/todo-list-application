9fa71eaeb6a54b2f15721746b29d001fe4106fd0
cse18107 <cse18107@cemk.ac.in>
2021-12-24 23:25:15 +0530

    working on list and form

--- client/src/components/Pages/AddTask/AddTask.js
+++ client/src/components/Pages/AddTask/AddTask.js
@@ -1,11 +1,61 @@
-import React from 'react'
+import React,{useState} from "react";
+import "./AddTask.css";
+import { CirclePicker } from 'react-color';
+const AddTask = () => {
 
-const AddTask = ()  => {
-    return (
-        <div style={{"background-color":"yellow","height":"100vw"}}>
-            Add Task
-        </div>
-    )
-}
+    
 
-export default AddTask
+    const showColor=(color)=>{
+        console.log(color.hex);
+    }
+  return (
+    <div className="add-task__container">
+      <div className="add-task__content">
+        <form class="row g-3">
+          <div class="col-12">
+            <label for="inputEmail4" class="form-label">
+              Title
+            </label>
+            <input
+              type="text"
+              class="form-control"
+              id="inputEmail4"
+              placeholder="Any title of your task"
+            />
+          </div>
+          <div class="col-12">
+            <label for="inputAddress" class="form-label">
+              Description
+            </label>
+            <input
+              type="text"
+              class="form-control"
+              id="inputEmail4"
+              placeholder="Description of your task"
+            />
+          </div>
+          <div class="col-12">
+            <label for="inputAddress2" class="form-label">
+              Date
+            </label>
+            <input
+              type="text"
+              class="form-control"
+              id="inputEmail4"
+              placeholder="DD/MM/YYY"
+            />
+          </div>
+           <label for="exampleColorInput" class="form-label">
+            Color picker
+          </label>
+          <CirclePicker onChangeComplete={showColor}/>
+          <div class="col-12">
+          <button type="button" class="btn btn-success">Success</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddTask;
--- client/src/components/Pages/Home/Home.js
+++ client/src/components/Pages/Home/Home.js
@@ -8,6 +8,46 @@ const DUMMY_VALUE = [
     date: "28/10/1998",
   },
   {
+    title: "Watching TV",
+    description: "Tom and Jerry",
+    date: "10/10/2022",
+  },
+  {
+    title: "Study",
+    description: "Operating System",
+    date: "23/4/2021",
+  },
+  {
+    title: "cooking",
+    description: "Kosa mangso",
+    date: "30/2/2020",
+  },
+  {
+    title: "Watching TV",
+    description: "Tom and Jerry",
+    date: "10/10/2022",
+  },
+  {
+    title: "Study",
+    description: "Operating System",
+    date: "23/4/2021",
+  },
+  {
+    title: "coding",
+    description: "Leetcode question practice",
+    date: "28/10/1998",
+  },
+  {
+    title: "Watching TV",
+    description: "Tom and Jerry",
+    date: "10/10/2022",
+  },
+  {
+    title: "Study",
+    description: "Operating System",
+    date: "23/4/2021",
+  },
+  {
     title: "cooking",
     description: "Kosa mangso",
     date: "30/2/2020",
@@ -29,17 +69,26 @@ const Home = () => {
     <div className="home-body">
       <div className="home-body__container">
         <div className="home-body__content">
-          {DUMMY_VALUE.map(() => {
-              return (<div className="home-body__content-body">
-            
-                <div className="home-body__content-body__content">
-                  <h1>
-                    Title: <span>Coding</span>
-                  </h1>
+          {DUMMY_VALUE.map((value) => {
+            return (
+              <div className="home-body__content-body">
+                <div className="card" style={{ width: "18rem" }}>
+                  <div className="card-body">
+                    <h5 className="card-title">{value.title}</h5>
+                    <p className="card-text">{value.description}</p>
+                    <div className="buttons">
+                    <button type="button" class="btn btn-success">
+                      Edit
+                    </button>
+                    <button type="button" class="btn btn-danger">
+                      Delete
+                    </button>
+                    </div>
+                  </div>
                 </div>
-             
-          </div> );
-            })}
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
